2a71514d502b8842834ddd24e8bc3d80c21f48bb
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-10-21 22:25:26 +0530

    js p10 activity

---
+++ javaScript/js p10 activity.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+    let head = document.querySelector("h1");
+    let randomColor = getRandomColor();
+    head.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+    console.log(`color updated`);
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
