584e458480f9d3350b5f5249a0834fe9e371d1c2
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-11-14 22:54:34 +0530

    backend 3 almost over

---
+++ ejs/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+app.set("view engine", "ejs"); //ejs internally aquired by express; no need to require
+app.set("views", path.join(__dirname, "/views")); //__dirname gives current working dir of index.js
+
+app.get("/", (req, res) => {
+    // res.send("this is root/home")
+    res.render("home.ejs");
+});
+
+app.get("/rolldice", (req, res) => {
+    // res.send("this is root/home")
+    let rollDice = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { rollDice });
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    console.log(username);
+    res.render("insta.ejs", { username });
+});
+
+app.listen(port, () => {
+    console.log("listening on port -", port);
+});
--- expressDir/index.js
+++ expressDir/index.js
@@ -4,5 +4,35 @@ const app = express();
 let port = 3000;
 
 app.listen(port, () => {
-    console.log("app is litening on port : ", port);
+    console.log("app is listening on port : ", port);
+});
+
+// app.use((req, resp) => {
+//     console.log("request recieved");
+//     resp.send({
+//         fruit: "apple",
+//         color: "red",
+//     });
+// });
+
+//or
+
+app.get("/", (req, res) => {
+    console.log(`please enter your username and id after localhost:_ _ _ _/`);
+    res.send(
+        `<h1>please enter your localhost:3000/<u><i>username</i></u>/<u><i>id</i></u></h1>`
+    );
+}); // "/" gives a default value
+app.get("/:username/:Id", (req, res) => {//path parameter/template
+    let { username, Id } = req.params;
+    console.log(req.params);
+    let welStr = `<h1>welcome to your page ${username}</h1>`;
+    res.send(welStr);
+}); //go to localhost on hoppscotch with localhost:3000/anything, it will be logged in as username
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    if (!q) {
+        res.send("<h1>nothing searched</h1>");
+    }
+    res.send(`search result for : ${q}`);
 });
