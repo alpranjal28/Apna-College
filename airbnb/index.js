cfa65958c25dfa3b695828511f07e55d6e8828c1
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-11-29 21:08:36 +0530

    major project phase1 completed

--- airbnb/index.js
+++ airbnb/index.js
@@ -3,9 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("../airbnb/models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -23,31 +26,53 @@ app.get("/", (req, res) => {
     res.send("hi, i am root");
 });
 
-//app.get("/testListing", (req, res) => {
-// let sampleListing = new Listing({
-//     title: "my new billa",
-//     description: "by the tree",
-//     price: 1322,
-//     location: "home",
-//     country: "muktipara",
-// });
-
-// sampleListing
-//     .save()
-//     .then((res) => {
-//         console.log(res);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-
-// res.send(`successful`)
-//});
-
+// Index route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("index.ejs", { allListings });
+    res.render("./listings/index.ejs", { allListings });
 });
+
+//new route
+app.get("/listings/new", async (req, res) => {
+    res.render("./listings/new.ejs");
+});
+
+//Show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", { listing });
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+    // let { title, description, image, price, country, location } = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", { listing });
+});
+
+// update route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id)
+    console.log("deleted listing", deletedListing);
+    res.redirect("/listings")
+})
 
 app.listen(8080, () => {
     console.log(`server is listening to port 8080`);
