27f83d46bbe221eb95ff4be32e0f36610f1198f4
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-12-06 23:16:28 +0530

    phase1 part c middlewares

--- airbnb/index.js
+++ airbnb/index.js
@@ -5,13 +5,14 @@ const Listing = require("../airbnb/models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public")));
 
 main()
     .then(() => {
@@ -48,12 +49,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //create route
-app.post("/listings", async (req, res) => {
-    // let { title, description, image, price, country, location } = req.body;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post(
+    "/listings",
+    wrapAsync(async (req, res, next) => {
+        // let { title, description, image, price, country, location } = req.body;
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+);
 
 //edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -75,6 +79,10 @@ app.delete("/listings/:id", async (req, res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log("deleted listing", deletedListing);
     res.redirect("/listings");
+});
+
+app.use((err, req, res, next) => {
+    res.send("something went wrong!");
 });
 
 app.listen(8080, () => {
