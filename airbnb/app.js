8312b9a76384d451e8d126269495fae724e9dc02
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-12-13 01:29:25 +0530

    found error at validating review

--- airbnb/app.js
+++ airbnb/app.js
@@ -47,19 +47,19 @@ const validateListing = (req, res, next) => {
     }
 };
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details
-            .map((el) => {
-                el.message;
-            })
-            .join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
+// const validateReview = (req, res, next) => {
+//     let { error } = reviewSchema.validate(req.body);
+//     if (error) {
+//         let errMsg = error.details
+//             .map((el) => {
+//                 el.message;
+//             })
+//             .join(",");
+//         throw new ExpressError(400, errMsg);
+//     } else {
+//         next();
+//     }
+// };
 
 // Index route
 app.get(
@@ -83,7 +83,7 @@ app.get(
     "/listings/:id",
     wrapAsync(async (req, res) => {
         let { id } = req.params;
-        const listing = await Listing.findById(id);
+        const listing = await Listing.findById(id).populate("reviews");
         res.render("./listings/show.ejs", { listing });
     })
 );
@@ -132,11 +132,10 @@ app.delete(
 );
 
 //reviews
+//post
 app.post(
-    "/listings/:id/reviews",
-    validateReview,
+    "/listings/:id/reviews", //validateReview,
     wrapAsync(async (req, res) => {
-        let { id } = req.params;
         let listing = await Listing.findById(req.params.id);
         let newReview = new Review(req.body.review);
 
@@ -144,8 +143,9 @@ app.post(
 
         await newReview.save();
         await listing.save();
+
         console.log("new review saved");
-        res.redirect(`/listings/${id}`);
+        res.redirect(`/listings/${listing._id}`);
     })
 );
 
