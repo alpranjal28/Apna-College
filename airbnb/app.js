ffbbe6e1ddaad162cb6ec4de89f1df69fbb2880c
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2023-12-28 11:18:31 +0530

    Remove ignored files

--- airbnb/app.js
+++ airbnb/app.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV != "production") {
-    require("dotenv").config();
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()
 }
 
 const express = require("express");
@@ -10,7 +10,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -20,7 +19,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
 main()
     .then(() => {
         console.log(`connected to DB`);
@@ -41,21 +39,8 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 60 * 60,
-});
-
-store.on("error", (err) => {
-    console.log("ERROR in MONGO SESSION STORE", err);
-});
-
 const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
+    secret: "mynotsosupersecretcode",
     resave: false,
     saveUninitialized: true,
     cookie: {
--- airbnb/controllers/listings.js
+++ airbnb/controllers/listings.js
@@ -48,7 +48,7 @@ module.exports.createListing = async (req, res, next) => {
     newListing.geometry = response.body.features[0].geometry;
 
     let savedListing = await newListing.save();
-    console.log(`saved new listing - ${savedListing.title}`);
+    console.log(savedListing);
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
